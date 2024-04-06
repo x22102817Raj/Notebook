2a8b90efb96a0c1e58e49d98c50dcb9005d70fa8
x22102817Raj <x22102817@student.ncirl.ie>
2024-04-06 18:00:54 +0100

    added staticfiles_Dirs

--- Notebook/settings.py
+++ Notebook/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+Staticfiles_DIRS = [ BASE_DIR / 'main/static' ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
